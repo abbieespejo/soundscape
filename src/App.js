b36f843ab6f42f58405417caa2ea6a52e59bfa4f
Abigail Espejo <espejoabig@machete.ecs.vuw.ac.nz>
2023-08-16 17:44:25 +1200

    Push existing web-app template to repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Element, scroller } from 'react-scroll';
+import { Box } from '@mui/material';
 import './App.css';
 
-function App() {
+const Section = ({ name }) => (
+  <Element name={name} className="element full-vh">
+    <h1>{name}</h1>
+  </Element>
+);
+
+const DotNavigation = ({ sectionNames, activeSection, setActiveSection }) => {
+  const handleClick = (section) => {
+    scroller.scrollTo(section, {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Box className="dot-nav">
+      {sectionNames.map((name, i) => (
+        <div
+          key={name}
+          onClick={() => handleClick(name)}
+          className={`dot ${activeSection === i ? 'active' : ''}`}
+        />
+      ))}
+    </Box>
+  );
+};
+
+const App = () => {
+  const sectionNames = ['Section 1', 'Section 2', 'Section 3'];
+  const [activeSection, setActiveSection] = useState(0);
+
+  const onScroll = (e) => {
+    const scrollPosition = e.currentTarget.scrollTop;
+    const currentIndex = Math.round(scrollPosition / window.innerHeight);
+    setActiveSection(currentIndex);
+  };
+
+  return (
+    <div className="App" onScroll={onScroll}>
+      <DotNavigation
+        sectionNames={sectionNames}
+        activeSection={activeSection}
+        setActiveSection={setActiveSection}
+      />
+      {sectionNames.map((name) => (
+        <Section key={name} name={name} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
