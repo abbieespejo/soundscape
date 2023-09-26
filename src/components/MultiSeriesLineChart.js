df5634bf3e83228f7eb4a49ec2e94ab1a429e5d6
Abigail Espejo <espejoabig@myvuw.ac.nz>
2023-09-26 15:50:05 +1300

    Fixed bug where data was populating more than once on webpage load

--- src/components/MultiSeriesLineChart.js
+++ src/components/MultiSeriesLineChart.js
@@ -1,83 +1,158 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, chartRef } from "react";
 import * as d3 from "d3";
 import dataset from "../data/dataset.csv";
+import "./MultiSeriesLineChart.css";
 
 const MultiSeriesLineChart = () => {
+  /* Styling for the rendered chart */
+  const width = 928;
+  const height = 500;
+  const marginTop = 20;
+  const marginRight = 30;
+  const marginBottom = 30;
+  const marginLeft = 40;
+  const chartRef = useRef(null);
+  const initialMount = useRef(true);
   const [data, setData] = useState([]);
-  const [selectedCharacteristic, setSelectedCharacteristic] =
-    useState("duration_ms");
-  const [averagedData, setAveragedData] = useState({});
-
-  // data manipulation
-
-  // for the genres edm, latin, pop, hip-hop, rock, and r&b, we need to find
-  // the average duration_ms, popularity, danceability, energy, key, loudness,
-  // speechiness, acousticness, instrumentalness, liveness, valence, tempo for
-  // each year spanning 1999-2019
-
-  // retrieve all songs for a certain genre by looking at the genre value of data entry (e.g. all pop songs)
-
-  // retrieve the selected audio characteristic value for all songs under the genre
-
-  // with these values, calculate the average (sum of terms / number of terms) for each year by looking at the 'year' field
-
-  const calculateAverage = (data) => {
-    const selectedCharacteristic = 'duration_ms'; // Hardcoded value
-    const genres = [
-      "Dance/Electronic",
-      "latin",
-      "pop",
-      "hip hop",
-      "rock",
-      "R&B",
-    ];
-    
-    // Retrieve unique years and sort them
-    const years = Array.from(new Set(data.map((d) => +d.year))).sort((a, b) => a - b);
-  
-    const avgData = {};
-  
-    years.forEach((year) => {
-      avgData[year] = {};  // Initialize an empty object for each year
+  const audioCharacteristics = [
+    "duration_ms",
+    "popularity",
+    "danceability",
+    "energy",
+    "loudness",
+    "speechiness",
+    "acousticness",
+    "instrumentalness",
+    "liveness",
+    "valence",
+    "tempo",
+  ];
+  const genres = ["Dance/Electronic", "latin", "pop", "hip hop", "rock", "R&B"];
+  const years = Array.from(new Set(data.map(d => +d.year)));  // retrieve the years for iteration & data storage
+  const [averagedData, setAveragedData] = useState([]);
+  const [selectedCharacteristic, setSelectedCharacteristic] = useState("duration_ms");
+  useEffect(() => {
+    console.log("Fetching data");
+    d3.csv(dataset).then((data) => {
+      setData(data);
     });
-  
+  }, []); // This will only run once on component mount to fetch the data
+
+  useEffect(() => {
+    if (data.length === 0) return; // Ensure data is loaded before calculating averages
+    
+    if (initialMount.current) {
+      initialMount.current = false; // It's initial mount, don't run the effect body yet.
+      return;
+    }
+    console.log("Processing data...");
+    const averages = {};
+    years.forEach((year) => {
+      averages[year] = {}; // Initialise an empty object for each year
+    });
     genres.forEach((genre) => {
       years.forEach((year) => {
         const filteredData = data.filter(
           (d) => d.genre.includes(genre) && +d.year === year
         );
-  
         const sum = filteredData.reduce(
           (acc, curr) => acc + +curr[selectedCharacteristic],
           0
         );
-  
         const avg = sum / filteredData.length || 0;
-        avgData[year][genre] = avg;  // Store the average value for each genre under the corresponding year
+        averages[year][genre] = avg; // Store the average value for each genre under the corresponding year
       });
     });
-  
-    console.log("Averaged Data:", avgData);
-  };
-  
-  useEffect(() => {
-    // Read and parse the CSV data
-    d3.csv(dataset).then((fetchedData) => {
-      setData(fetchedData);
-      // Calculate average for the initial characteristic
-      calculateAverage(fetchedData);
-    });
-  }, []);
-  
+    setAveragedData(averages);
+    console.log(averages);
+  }, [data, selectedCharacteristic]);
+
+  // useEffect(() => {
+  //   const svg = d3.select(chartRef.current)
+  //     .append("svg")
+  //     .attr("width", width)
+  //     .attr("height", height);
+  //   const x = d3
+  //     .scaleLinear()
+  //     .domain(d3.extent(years))
+  //     .range([marginLeft, width - marginRight]);
+
+  //   const allAverages = Object.values(averagedData).flatMap((year) =>
+  //     Object.values(year)
+  //   );
+  //   const minVal = d3.min(allAverages);
+  //   const maxVal = d3.max(allAverages);
+
+  //   const y = d3
+  //     .scaleLinear()
+  //     .domain([minVal, maxVal])
+  //     .range([height - marginBottom, marginTop]);
+
+  //   const line = d3.line()
+  //     .x(d => x(d.year))
+  //     .y(d => y(d.value));
+
+  //   // Render each genre as a line on the chart
+  //   if (Object.keys(averagedData).length > 0) {
+  //     genres.forEach((genre, index) => {
+  //       svg
+  //         .append("path")
+  //         .datum(years)
+  //         .attr("fill", "none")
+  //         .attr("stroke", d3.schemeCategory10[index]) // color based on genre's index
+  //         .attr("stroke-width", 1.5)
+  //         .attr("d", d => line(d.map(year => ({
+  //           year: year,
+  //           value: averagedData[year][genre]
+  //         }))));
+
+  //     });
+  //   }
+  //   // X-Axis
+  //   svg
+  //     .append("g")
+  //     .attr("transform", `translate(0,${height - marginBottom})`)
+  //     .call(
+  //       d3
+  //         .axisBottom(x)
+  //         .ticks(width / 80)
+  //         .tickSizeOuter(0)
+  //     );
+  //   // Y-Axis
+  //   svg
+  //     .append("g")
+  //     .attr("transform", `translate(${marginLeft},0)`)
+  //     .call(d3.axisLeft(y).ticks(height / 40))
+  //     .call((g) => g.select(".domain").remove())
+  //     .call((g) =>
+  //       g
+  //         .selectAll(".tick line")
+  //         .clone()
+  //         .attr("x2", width - marginLeft - marginRight)
+  //         .attr("stroke-opacity", 0.1)
+  //     )
+  //     .call((g) =>
+  //       g
+  //         .append("text")
+  //         .attr("x", -marginLeft)
+  //         .attr("y", 10)
+  //         .attr("fill", "currentColor")
+  //         .attr("text-anchor", "start")
+  //         .text(`↑ Average ${selectedCharacteristic}`)
+  //     );
+
+  // });
 
   return (
-    <div>
+    <div id="line-chart-container" ref={chartRef}>
+      {/* <h1>Audio Characteristics Average Over the Years</h1>
       <select onChange={(e) => setSelectedCharacteristic(e.target.value)}>
-        <option value="duration_ms">Duration</option>
-        <option value="popularity">Popularity</option>
-        {/* Add other options here */}
-      </select>
-      {/* Here, you can use D3 to render your multi-series line chart based on `averagedData` */}
+        {audioCharacteristics.map((characteristic) => (
+          <option key={characteristic} value={characteristic}>
+            {characteristic}
+          </option>
+        ))}
+      </select> */}
     </div>
   );
 };
